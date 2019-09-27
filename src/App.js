caf400bbfb31c1d859d097135e6d15e0931e98b4
taeyzzz <tkul@sertiscorp.com>
2019-09-27 11:18:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        NODE_ENV: {process.env.NODE_ENV}
+      </div>
+      <div>
+        REACT_APP_SERVER_URL: {process.env.REACT_APP_SERVER_URL}
+      </div>
+      <div>
+        REACT_APP_ENVIRONMENT: {process.env.REACT_APP_ENVIRONMENT}
+      </div>
+      <button>This is the best button</button>
     </div>
   );
 }
