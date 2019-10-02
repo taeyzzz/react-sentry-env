9ab62ff52caeb0076efa6652910d0983f5f945b2
taeyzzz <tkul@sertiscorp.com>
2019-10-02 10:35:55 +0700

    Updates

---
+++ precache-manifest.164a9044590d60f32ba4f67c06efdf01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e893a4ce363216e6e30e3f726a3660",
+    "url": "/react-sentry-env/index.html"
+  },
+  {
+    "revision": "cca45c2f88c5ee14919e",
+    "url": "/react-sentry-env/static/css/main.0cb14e4e.chunk.css"
+  },
+  {
+    "revision": "43ccefa9edc3c4a035a6",
+    "url": "/react-sentry-env/static/js/2.b374e249.chunk.js"
+  },
+  {
+    "revision": "cca45c2f88c5ee14919e",
+    "url": "/react-sentry-env/static/js/main.f0e1061b.chunk.js"
+  },
+  {
+    "revision": "5a25f183bfc20c04477a",
+    "url": "/react-sentry-env/static/js/runtime-main.65bdc389.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-sentry-env/static/media/logo.25bf045c.svg"
+  }
+]);
